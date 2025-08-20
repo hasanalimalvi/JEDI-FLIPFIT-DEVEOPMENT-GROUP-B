b9d8f9c442800cc66d95e292d6c92fb95cd7a8ec
Chanukya-Maruthi <chanukyamaruthi75@gmail.com>
2025-08-20 09:39:03 +0530

    Added Entry Point Class for all Menu

---
+++ JEDI-FLIPFIT-JAVA-POS/src/main/java/com/flipfit/client/Application.java
@@ -0,0 +1,7 @@
+package com.flipfit.client;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Main Entry Point for all types of Menu");
+    }
+}
