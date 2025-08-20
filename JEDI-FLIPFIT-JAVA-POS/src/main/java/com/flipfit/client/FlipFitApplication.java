0cf2740774a04b204a0a9e3c2b7c736b2c99e7a7
hasanalimalvi <hasanalimalvi10@gmail.com>
2025-08-20 10:00:38 +0530

    Renamed the application to FlipFitApplication.

--- JEDI-FLIPFIT-JAVA-POS/src/main/java/com/flipfit/client/FlipFitApplication.java
+++ JEDI-FLIPFIT-JAVA-POS/src/main/java/com/flipfit/client/FlipFitApplication.java
@@ -1,6 +1,6 @@
 package com.flipfit.client;
 
-public class Application {
+public class FlipFitApplication {
     public static void main(String[] args) {
         System.out.println("Main Entry Point for all types of Menu");
     }
