b0f34d1558a4a8a8dcd2137060d7e47421904d02
Chanukya-Maruthi <chanukyamaruthi75@gmail.com>
2025-08-22 14:02:02 +0530

    Changed text for payment failure

--- JEDI-FLIPFIT-JAVA-POS/src/main/java/com/flipfit/exception/PaymentFailedException.java
+++ JEDI-FLIPFIT-JAVA-POS/src/main/java/com/flipfit/exception/PaymentFailedException.java
@@ -5,7 +5,7 @@ public class PaymentFailedException extends Exception {
     private String message;
 
     public PaymentFailedException(String bookingId) {
-        this.message = "Payment already done for Booking Id: " + bookingId + ".";
+        this.message = "Payment failed for Booking Id: " + bookingId + ".";
     }
 
     public String getMessage() {
