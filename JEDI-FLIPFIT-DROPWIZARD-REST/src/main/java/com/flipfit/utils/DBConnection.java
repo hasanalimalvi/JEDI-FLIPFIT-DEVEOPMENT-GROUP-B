f83224a95300a19e5105c41dd19a9919fd4a1e59
Hasanali Malvi <105169166+hasanalimalvi@users.noreply.github.com>
2025-08-25 21:04:40 +0530

    Update DBConnection.java

--- JEDI-FLIPFIT-DROPWIZARD-REST/src/main/java/com/flipfit/utils/DBConnection.java
+++ JEDI-FLIPFIT-DROPWIZARD-REST/src/main/java/com/flipfit/utils/DBConnection.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/FlipFit_Schema";
     private static final String USER = "root";
-    private static final String PASSWORD = "hasan@123";
+    private static final String PASSWORD = "Enter your password here";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
